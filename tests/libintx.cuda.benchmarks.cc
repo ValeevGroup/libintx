1021e601a8eb338160eae266ca9c060cc39b97c9
asadchev <asadchev@gmail.com>
2020-09-13 20:57:38 -0400

    CUDA 3-center Obara-Saika

--- tests/libintx.cuda.benchmarks.cc
+++ tests/libintx.cuda.benchmarks.cc
@@ -1,2 +1,117 @@
+#include "libintx/engine/os/engine.h"
+#include "libintx/cuda/eri.h"
+#include "libintx/utility.h"
+#include "test.h"
+#include <iostream>
+
+using namespace libintx;
+using namespace libintx::cuda;
+using libintx::time;
+
+const Double<3> r0 = {  0.7, -1.2, -0.1 };
+const Double<3> r1 = { -1.0,  0.0,  0.3 };
+const Double<3> rx = {  0.5, -1.5,  0.9 };
+
+auto eri_test_case(int A, int B, int X, std::vector< std::array<int,2> > Ks = { {1,1} }, int N = 0) {
+
+  printf("# %i%i%i\n", A, B, X);
+  std::vector<double> ratios;
+
+  for (auto K : Ks) {
+
+  int Nk = N;
+  if (!Nk) Nk = 20000000/(K[1]*npure(A+B)*npure(X));
+
+  auto a = test::gaussian(A, K[0]);
+  auto b = test::gaussian(B, K[1]);
+  auto x = test::gaussian(X, 1);
+
+  auto gpu = libintx::cuda::eri<3>();
+
+  auto centers = std::vector< Double<3> >{r0,r1,rx};
+  gpu->set_centers(centers);
+
+  int AB = ncart(a)*ncart(b);
+  int nbf = AB*npure(x);
+
+  auto buffer = device::vector<double>(Nk*nbf);
+
+  double tgpu = 0;
+  for (int k = 0; k < 1; ++k) {
+    IntegralList<3> list(Nk);
+    for (int i = 0; i < list.size(); ++i) {
+      list[i] = { {0,1,2}, buffer.data() + i*nbf };
+    }
+    auto t0 = time::now();
+    gpu->compute(a, b, x, list);
+    device::synchronize();
+    double t = time::since(t0);
+    tgpu = std::max(tgpu,1/t);
+  }
+
+  printf("# K=%i: ", K[0]*K[1]);
+  printf("T(gpu)=%f ", 1/tgpu);
+
+  double ratio = 0;
+  {
+    auto cpu = libintx::os::eri(a,b,x);
+    auto t0 = time::now();
+    for (int i = 0; i < Nk; ++i) {
+      cpu->compute(r0, r1, rx);
+    }
+    auto t = time::since(t0);
+    printf("T(cpu)=%f ", t);
+    ratio = t*tgpu;
+  }
+
+  printf("T(cpu)/T(gpu)=%f\n", ratio);
+  ratios.push_back(ratio);
+
+  }
+
+  printf("%i%i%i, ", A, B, X);
+  for (auto r : ratios) {
+    printf("%f, ", r);
+  }
+  printf("\n");
+
+}
+
+#define ERI_TEST_CASE(I,J,K,...)                                     \
+  if (test::enabled(I,J,K)) { eri_test_case(I,J,K,__VA_ARGS__); }
+
+
 int main() {
+
+  std::vector< std::array<int,2> > K = { {1,1}, {5,1}, {5,5} };
+
+  ERI_TEST_CASE(0,0,0, K);
+  ERI_TEST_CASE(1,0,0, K);
+  ERI_TEST_CASE(2,0,0, K);
+  ERI_TEST_CASE(3,0,0, K);
+  ERI_TEST_CASE(4,0,0, K);
+  ERI_TEST_CASE(5,0,0, K);
+  ERI_TEST_CASE(6,0,0, K);
+
+  ERI_TEST_CASE(1,0,1, K);
+  ERI_TEST_CASE(2,0,2, K);
+  ERI_TEST_CASE(3,0,3, K);
+  ERI_TEST_CASE(4,0,4, K);
+  ERI_TEST_CASE(5,0,5, K);
+  ERI_TEST_CASE(6,0,6, K);
+
+  ERI_TEST_CASE(1,1,0, K);
+  ERI_TEST_CASE(2,2,0, K);
+  ERI_TEST_CASE(3,3,0, K);
+  ERI_TEST_CASE(4,4,0, K);
+  ERI_TEST_CASE(5,5,0, K);
+  ERI_TEST_CASE(6,6,0, K);
+
+  ERI_TEST_CASE(1,1,1, K);
+  ERI_TEST_CASE(2,2,2, K);
+  ERI_TEST_CASE(3,3,3, K);
+  ERI_TEST_CASE(4,4,4, K);
+  ERI_TEST_CASE(5,5,5, K);
+  ERI_TEST_CASE(6,6,6, K);
+
 }
--- tests/libintx.cuda.test.cc
+++ tests/libintx.cuda.test.cc
@@ -1,2 +1,125 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "test.h"
+
+#include "libintx/cuda/eri.h"
+#include "libintx/engine/os/engine.h"
+#include "libintx/reference.h"
+#include "libintx/utility.h"
+
+using namespace libintx;
+using namespace libintx::cuda;
+
+template<size_t N>
+auto integral_list(size_t n) {
+  return IntegralList<N>(n);
+}
+
+constexpr double tolerance = 1e-7;
+
+const Double<3> r0 = {  0.7, -1.2, -0.1 };
+const Double<3> r1 = { 0.75,  3.14,  -0.237 };
+const Double<3> rx = {  0.5, -1.5,  0.9 };
+
+//const Double<3> r0 = {  0, 0, 0 };
+//const Double<3> r1 = {  0, 0, 0 };
+//const Double<3> rx = {  0, 0, 0 };
+
+auto obara_saika_eri3_test_case(
+  int A, int B, int X,
+  std::array<int,2> K = {1,1}, int N = 1)
+{
+
+  if (K[0]*K[1] > 1 && A+B+X >= 3*LMAX) return;
+
+  auto a = test::gaussian(A, K[0], false);
+  auto b = test::gaussian(B, K[1], false);
+  auto x = test::gaussian(X, 1, true);
+
+  auto engine = libintx::cuda::eri<3>();
+
+  auto centers = std::vector< Double<3> >{r0,r1,rx};
+  engine->set_centers(centers);
+
+  int AB = nbf(a)*nbf(b);
+  auto result = host::vector<double>(N*nbf(x)*AB);
+
+  IntegralList<3> list(N);
+  for (int i = 0; i < N; ++i) {
+    double *output = result.data() + i*nbf(x)*AB;
+    double scale = 1.0;
+    list[i] = { {0,1,2}, output, scale };
+  }
+
+  engine->compute(a, b, x, list);
+  device::synchronize();
+
+  auto ref = libintx::reference::eri(a,b,x);
+  ref->compute(r0, r1, rx);
+
+  // auto ref = libintx::os::eri(a,b,x);
+  // ref->compute(r0, r1, rx);
+
+  for (int i = 0; i < AB; ++i) {
+    for (int k = 0; k < nbf(x); ++k) {
+      auto ab_x = result[k+i*nbf(x)];
+      test::ReferenceValue ab_x_reference(
+        ref->buffer()[k+i*nbf(x)],
+        tolerance,
+        i%nbf(a), i/nbf(b), k
+      );
+      CHECK(ab_x == ab_x_reference);
+    }
+  }
+
+}
+
+#define ERI3_TEST_CASE(F,I,J,K,...)				\
+  if (test::enabled(I,J,K)) {					\
+    for (int ka : { 1, 5 }) {					\
+      for (int kb : { 1, 5 }) {					\
+	SUBCASE(str("(ab|x)=",I,J,K,",K=",ka*kb).c_str()) {	\
+          F(I, J, K, {ka,kb});					\
+        }							\
+      }								\
+    }								\
+  }
+
+TEST_CASE("obara-saika.eri3") {
+  // for (size_t x = 0; x <= XMAX; ++x) {
+  //   for (size_t a = 0; a <= LMAX; ++a) {
+  //     for (size_t b = 0; b <= a; ++b) {
+  //       ERI3_TEST_CASE(obara_saika_eri3_test_case, a,b,x);
+  //     }
+  //   }
+  // }
+  
+  ERI3_TEST_CASE(obara_saika_eri3_test_case,0,0,0);
+  ERI3_TEST_CASE(obara_saika_eri3_test_case,1,0,0);
+  ERI3_TEST_CASE(obara_saika_eri3_test_case,2,0,0);
+  ERI3_TEST_CASE(obara_saika_eri3_test_case,3,0,0);
+  ERI3_TEST_CASE(obara_saika_eri3_test_case,4,0,0);
+  ERI3_TEST_CASE(obara_saika_eri3_test_case,5,0,0);
+  ERI3_TEST_CASE(obara_saika_eri3_test_case,6,0,0);
+
+  ERI3_TEST_CASE(obara_saika_eri3_test_case,1,0,1);
+  ERI3_TEST_CASE(obara_saika_eri3_test_case,2,0,2);
+  ERI3_TEST_CASE(obara_saika_eri3_test_case,3,0,3);
+  ERI3_TEST_CASE(obara_saika_eri3_test_case,4,0,4);
+  ERI3_TEST_CASE(obara_saika_eri3_test_case,5,0,5);
+  ERI3_TEST_CASE(obara_saika_eri3_test_case,6,0,6);
+
+  ERI3_TEST_CASE(obara_saika_eri3_test_case,1,1,0);
+  ERI3_TEST_CASE(obara_saika_eri3_test_case,2,2,0);
+  ERI3_TEST_CASE(obara_saika_eri3_test_case,3,3,0);
+  ERI3_TEST_CASE(obara_saika_eri3_test_case,4,4,0);
+  ERI3_TEST_CASE(obara_saika_eri3_test_case,5,5,0);
+  ERI3_TEST_CASE(obara_saika_eri3_test_case,6,6,0);
+
+  ERI3_TEST_CASE(obara_saika_eri3_test_case,1,1,1);
+  ERI3_TEST_CASE(obara_saika_eri3_test_case,2,2,2);
+  //ERI3_TEST_CASE(obara_saika_eri3_test_case,3,3,3);
+  ERI3_TEST_CASE(obara_saika_eri3_test_case,4,4,4);
+  ERI3_TEST_CASE(obara_saika_eri3_test_case,5,5,5);
+  ERI3_TEST_CASE(obara_saika_eri3_test_case,6,6,6);
+
+}
