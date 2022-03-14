315765bcc2172779ea3f66d9614933c6ef5010d5
asadchev <asadchev@gmail.com>
2022-03-14 18:46:46 -0400

    Forward header src/libintx/forward.h

---
+++ src/libintx/forward.h
@@ -0,0 +1,16 @@
+#ifndef LIBINTX_FORWARD_H
+#define LIBINTX_FORWARD_H
+
+#define LIBINTX_NOINLINE __attribute__((noinline))
+
+#ifdef __CUDACC__
+#define LIBINTX_GPU_ENABLED __host__ __device__
+#define LIBINTX_GPU_FORCEINLINE __forceinline__
+#define LIBINTX_GPU_CONSTANT __constant__
+#else
+#define LIBINTX_GPU_ENABLED
+#define LIBINTX_GPU_FORCEINLINE inline
+#define LIBINTX_GPU_CONSTANT
+#endif
+
+#endif /* LIBINTX_FORWARD_H */
