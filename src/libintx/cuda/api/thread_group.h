a0e45139557bb38e497cd5948fdcd433b2bf03f4
asadchev <asadchev@gmail.com>
2023-07-14 14:48:16 -0400

    CUDA thread_block with compile-time dims

--- src/libintx/cuda/api/thread_group.h
+++ src/libintx/cuda/api/thread_group.h
@@ -4,40 +4,77 @@
 #include <cooperative_groups.h>
 
 namespace libintx::cuda {
-  using cooperative_groups::thread_block;
+
   using cooperative_groups::thread_group;
+  using cooperative_groups::tiled_partition;
   using cooperative_groups::this_thread_block;
-  using cooperative_groups::coalesced_threads;
+
+  template<int X, int Y = 1, int Z = 1>
+  struct thread_block {
+    static constexpr int x = X;
+    static constexpr int y = Y;
+    static constexpr int z = Z;
+    __device__ static constexpr int size() { return x*y*z; }
+    __device__ static constexpr auto thread_rank() {
+      return this_thread_block().thread_rank();
+    }
+    __device__ static void sync() {
+      this_thread_block().sync();
+    }
+    constexpr operator dim3() const {
+      return dim3{ x, y, z };
+    }
+    __device__ operator thread_group() const {
+      return this_thread_block();
+    }
+  };
+
+  struct this_warp {
+    __device__ static constexpr int size() { return 32; }
+    __device__ static auto thread_rank() {
+      return this_thread_block().thread_rank()%32;
+    }
+    __device__ static void sync() {
+      __syncwarp();
+    }
+  };
+
+  template<int ... Dims>
+  __device__ __forceinline__
+  constexpr auto this_thread_block() {
+    return thread_block<Dims...>{};
+  }
+
 }
 
 namespace libintx::cuda {
 
   template<typename T, typename V = T>
   __device__
-  void fill(size_t size, T *dst, V v, const thread_group &g) {
-    for (int i = g.thread_rank(); i < size; i += g.size()) {
+  void fill(size_t size, T *dst, V v, const auto &thread_group) {
+    for (int i = thread_group.thread_rank(); i < size; i += thread_group.size()) {
       dst[i] = v;
     }
   }
 
   template<typename T>
-  __device__
-  void memcpy(size_t size, const T *src, T *dst, const thread_group &block) {
-    for (int i = block.thread_rank(); i < size; i += block.size()) {
+  __device__ __forceinline__
+  void memcpy(size_t size, const T *src, T *dst, const auto &thread_group) {
+    for (int i = thread_group.thread_rank(); i < size; i += thread_group.size()) {
       dst[i] = src[i];
     }
   }
 
   template<typename T>
-  __device__
-  void memcpy1(const T *src, T *dst, const thread_group &block) {
+  __device__ __forceinline__
+  void memcpy1(const T *src, T *dst, const auto &thread_group) {
     using byte4 = uint32_t;
     static_assert(sizeof(T)%sizeof(byte4) == 0);
     memcpy(
       sizeof(T)/sizeof(byte4),
       reinterpret_cast<const byte4*>(src),
       reinterpret_cast<byte4*>(dst),
-      block
+      thread_group
     );
   }
 
